23cd9e2896bae541636a96b73cb529148b4b8fa5
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-13 22:32:40 +0300

    lab completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
+const img = screen.queryByAltText("I made this");
 import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>I Made This</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
